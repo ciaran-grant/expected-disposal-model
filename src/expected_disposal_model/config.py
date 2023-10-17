940513c002a2b3f923e49887ba1f0f599fe0733f
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2023-10-17 21:12:13 +1100

    update config and predict model function

--- src/expected_disposal_model/config.py
+++ src/expected_disposal_model/config.py
@@ -9,6 +9,9 @@ modelling_file_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/exp
 
 # Model
 model_output_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-disposal-model/model_outputs/models"
+model_v1_file_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-disposal-model/model_outputs/models/disposal_v1.joblib"
+
 
 # Scored Data
 prediction_output_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-disposal-model/data/predictions"
+scored_disposal_output = "/Users/ciaran/Documents/Projects/AFL/git-repositories/expected-disposal-model/data/predictions/disposal_scored_chains.csv"
--- src/expected_disposal_model/scoring/predict_model.py
+++ src/expected_disposal_model/scoring/predict_model.py
@@ -2,64 +2,49 @@ import pandas as pd
 import numpy as np
 import joblib
 
-from features.preprocessing import get_expected_scores, convert_chains_to_schema, calculate_exp_vaep_values, create_gamestate_labels
+from expected_disposal_model.data_preparation.preprocessing import convert_chains_to_schema, filter_disposals, create_labels
+from expected_disposal_model.config import raw_file_path, preprocessor_file_path, model_v1_file_path, scored_disposal_output
 
-from config import *   
-
-def predict_model():
-
-    expected_scores_path_dict = {
-    'set':{'goal':{'preprocessor':exp_goal_set_preprocessor_file_path,
-                    'model':exp_goal_set_model_file_path},
-            'behind':{'preprocessor':exp_behind_set_preprocessor_file_path,
-                    'model':exp_behind_set_model_file_path},
-            'miss':{'preprocessor':exp_miss_set_preprocessor_file_path,
-                    'model':exp_miss_set_model_file_path}},
-    'open':{'goal':{'preprocessor':exp_goal_open_preprocessor_file_path,
-                    'model':exp_goal_open_model_file_path},
-            'behind':{'preprocessor':exp_behind_open_preprocessor_file_path,
-                    'model':exp_behind_open_model_file_path},
-            'miss':{'preprocessor':exp_miss_open_preprocessor_file_path,
-                    'model':exp_miss_open_model_file_path}}
-    }
+def predict_model(raw_file_path, preprocessor_file_path, model_file_path, output_path):
 
     # Load data
-    chains = pd.read_csv(chain_file_path)
+    chains = pd.read_csv(raw_file_path)
     print("Chain data loaded.")
 
     # Processing
-    preproc = joblib.load(exp_vaep_preprocessor_file_path)
+    preproc = joblib.load(preprocessor_file_path)
     chain_features = preproc.transform(chains)
     
-    chains = get_expected_scores(chains, expected_scores_path_dict)
     schema_chains = convert_chains_to_schema(chains)
-    exp_vaep_labels = create_gamestate_labels(schema_chains)
-    schema_chains = pd.concat([schema_chains, chain_features, exp_vaep_labels], axis=1)
-    schema_chains = schema_chains.rename(columns={'exp_scores':'exp_scores_label',
-                                                  'exp_concedes':'exp_concedes_label'})
+    disposals = filter_disposals(schema_chains)
+    
+    labels = create_labels(schema_chains)
+    
+    schema_chains = pd.concat([disposals, chain_features, labels], axis=1)
     
     print("Preprocessing.. Complete.")
 
     # Load model
-    exp_score_model = joblib.load(exp_vaep_score_model_file_path)
-    schema_chains['exp_scores'] = np.clip(exp_score_model.predict(chain_features), 0, 6)
-    exp_concede_model = joblib.load(exp_vaep_concede_model_file_path)
-    schema_chains['exp_concedes'] = np.clip(exp_concede_model.predict(chain_features), 0, 6)
-
-    # Scoring
-    scored_chains = calculate_exp_vaep_values(schema_chains)
+    exp_disposal_model = joblib.load(model_file_path)
+    
+    # Scoring Model
+    schema_chains['xDisposal'] = exp_disposal_model.predict(chain_features)
     print("Scoring.. complete.")
     
     # Merge back to chains
-    chains = chains.merge(scored_chains.drop(columns = ['xScore']), how = "left", left_on=['Match_ID', 'Chain_Number', 'Order'], right_on=['match_id', 'chain_number', 'order'])
+    chains = chains.merge(schema_chains, how = "left", left_on=['Match_ID', 'Chain_Number', 'Order'], right_on=['match_id', 'chain_number', 'order'])
 
     # Export data
-    chains.to_csv(exp_vaep_chain_output_path, index=False)
+    chains.to_csv(output_path, index=False)
     print("Exporting.. complete.")
 
 if __name__ == "__main__":
     
-    predict_model()
+    predict_model(raw_file_path,
+                  preprocessor_file_path,
+                  model_v1_file_path,
+                  scored_disposal_output
+                  )
     
     
     
